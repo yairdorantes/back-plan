f890dbc2c06a34b87f7dc30f24400fa12313e8a6
yair <yairmasterlol@gmail.com>
2023-01-01 15:08:38 -0600

    xd

--- src/app.js
+++ src/app.js
@@ -2,22 +2,16 @@ import express from "express";
 import cors from "cors";
 import Routes from "./routes/Plants.routes.js";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://singular-torrone-c436c2.netlify.app",
+      "https://other-allowed-domain.com",
+    ],
+  })
+);
+
 app.use(express.json({ limit: "50mb" }));
-app.use(
-  "/api",
-  proxy({
-    target: "https://singular-torrone-c436c2.netlify.app/",
-    changeOrigin: true,
-  })
-);
-app.use(
-  "/*",
-  proxy({
-    target: "https://singular-torrone-c436c2.netlify.app/",
-    changeOrigin: true,
-  })
-);
 
 app.use("/api/", Routes);
 
