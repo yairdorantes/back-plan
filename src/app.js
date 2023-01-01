6bcacf79978bc74045f6d63c95dfa89ab8735cb0
yair <yairmasterlol@gmail.com>
2023-01-01 14:51:05 -0600

    d

--- src/app.js
+++ src/app.js
@@ -4,6 +4,21 @@ import Routes from "./routes/Plants.routes.js";
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(
+  "/api",
+  proxy({
+    target: "https://singular-torrone-c436c2.netlify.app/",
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/*",
+  proxy({
+    target: "https://singular-torrone-c436c2.netlify.app/",
+    changeOrigin: true,
+  })
+);
+
 app.use("/api/", Routes);
 
 // app.use("/api/", commerceRoutes);
