b302766a1d1f6cd3d7b6d88f5786bace6eed8d38
yair <yairmasterlol@gmail.com>
2023-01-01 16:15:14 -0600

    xd

--- src/app.js
+++ src/app.js
@@ -2,14 +2,7 @@ import express from "express";
 import cors from "cors";
 import Routes from "./routes/Plants.routes.js";
 const app = express();
-app.use(
-  cors({
-    origin: [
-      "https://singular-torrone-c436c2.netlify.app",
-      "https://other-allowed-domain.com",
-    ],
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 
